21c8915286e48c23389bb5391949c5c0fcdcde04
[prasanna] <prasanna>
2022-05-29 13:49:29 +0530

    [prasanna]: calculating float value

--- MaximumNumber/MaxValue.cs
+++ MaximumNumber/MaxValue.cs
@@ -63,5 +63,60 @@ namespace MaximumNumber
             }
            
         }
+        public double MaximumofFloatValue(double firstvalue, double secondvalue, double thirdvale)
+        {
+            if (firstvalue > secondvalue && firstvalue > thirdvale)
+            {
+                return firstvalue;
+
+            }
+            else if (secondvalue > firstvalue && secondvalue > thirdvale)
+            {
+                return secondvalue;
+
+            }
+            else
+            {
+                return thirdvale;
+            }
+
+        }
+        public double maximumofSecFloatvalue(double firstvalue, double secondvalue, double thirdvale)
+        {
+            if (firstvalue > secondvalue && firstvalue > thirdvale)
+            {
+                return firstvalue;
+
+            }
+            else if (secondvalue > firstvalue && secondvalue > thirdvale)
+            {
+                return secondvalue;
+
+            }
+            else
+            {
+                return thirdvale;
+            }
+
+
+        }
+        public double maximumofThirdFloatValue(double firstvalue, double secondvalue, double thirdvale)
+        {
+            if (firstvalue > secondvalue && firstvalue > thirdvale)
+            {
+                return firstvalue;
+
+            }
+            else if (secondvalue > firstvalue && secondvalue > thirdvale)
+            {
+                return secondvalue;
+
+            }
+            else
+            {
+                return thirdvale;
+            }
+
+        }
     }
 }
--- TestingMaxValue/UnitTest1.cs
+++ TestingMaxValue/UnitTest1.cs
@@ -3,11 +3,12 @@ namespace TestingMaxValue
     public class UnitTest1
     {
         [Test]
+
         public void GivenMaxInFirstPosition_CheckMaxNumber_ReturnMaxInFirstPosition()
         {
-            
-            double A = 100.5, B = 90.67, C = 54.45;
-            MaximumNumber.MaxFloatValue<double> example = new MaximumNumber.MaxFloatValue<double>();
+
+            int A = 10, B = 9, C = 5;
+            MaximumNumber.MaxValue<int> example = new MaximumNumber.MaxValue<int>();
             var ActuvalResult = example.maximumofintegers(A, B, C);
             Assert.AreEqual(A, ActuvalResult);
 
@@ -17,8 +18,8 @@ namespace TestingMaxValue
         public void GivenMaxInSecondPosition_CheckMaxNumber_ReturnMaxInsecondPosition()
         {
 
-            double A = 90.67, B = 100.5, C = 54.45;
-            MaximumNumber.MaxFloatValue<double> example = new MaximumNumber.MaxFloatValue<double>();
+            int A = 9, B = 10, C = 5;
+            MaximumNumber.MaxValue<int> example = new MaximumNumber.MaxValue<int>();
             var ActuvalResult = example.maximumofSecintegers(A, B, C);
             Assert.AreEqual(B, ActuvalResult);
 
@@ -28,12 +29,47 @@ namespace TestingMaxValue
         public void GivenMaxInThirsPosition_CheckMaxNumber_ReturnMaxInThirdPosition()
         {
 
-            double A = 90.67, B = 54.45, C = 100.5;
-            MaximumNumber.MaxFloatValue<double> example = new MaximumNumber.MaxFloatValue<double>();
+            int A = 9, B = 5, C = 10;
+            MaximumNumber.MaxValue<int> example = new MaximumNumber.MaxValue<int>();
             var ActuvalResult = example.maximumofThirdintegers(A, B, C);
+            Assert.AreEqual(C, ActuvalResult);
+
+
+        }
+        [Test]
+        public void GivenMaxValueInFirstPosition_CheckMaxNumber_ReturnMaxValueInFirstPosition()
+        {
+
+            double A = 100.5, B = 90.67, C = 54.45;
+            MaximumNumber.MaxValue<double> example = new MaximumNumber.MaxValue<double>();
+            var ActuvalResult = example.MaximumofFloatValue(A, B, C);
+            Assert.AreEqual(A, ActuvalResult);
+
+
+        }
+        [Test]
+        public void GivenMaxValueInSecondPosition_CheckMaxNumber_ReturnMaxValueInsecondPosition()
+        {
+
+            double A = 90.67, B = 100.5, C = 54.45;
+            MaximumNumber.MaxValue<double> example = new MaximumNumber.MaxValue<double>();
+            var ActuvalResult = example.maximumofSecFloatvalue(A, B, C);
+            Assert.AreEqual(B, ActuvalResult);
+
+
+        }
+        [Test]
+        public void GivenMaxValueInThirsPosition_CheckMaxNumber_ReturnMaxValueInThirdPosition()
+        {
+
+            double A = 90.67, B = 54.45, C = 100.5;
+            MaximumNumber.MaxValue<double> example = new MaximumNumber.MaxValue<double>();
+            var ActuvalResult = example.maximumofThirdFloatValue(A, B, C);
             Assert.AreEqual(C, ActuvalResult);
 
 
         }
     }
 }
+
+
