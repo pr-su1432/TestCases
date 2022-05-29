95a3ec5ce426f5117833aa0c661cbe773191aa6d
pr-su1432 <103477040+pr-su1432@users.noreply.github.com>
2022-05-29 13:56:52 +0530

    Delete Program.cs

--- MaximumNumber/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-MaximumNumber.MaxFloatValue<double> example = new MaximumNumber.MaxFloatValue<double>();
-Console.WriteLine(example.MaximumofFloatValue( 100.5, 90.67,54.45));
-Console.WriteLine(example.maximumofSecFloatvalue(90.67, 100.5, 54.45));
-Console.WriteLine(example.maximumofThirdFloatValue(90.67, 54.45, 100.5));
-
-
